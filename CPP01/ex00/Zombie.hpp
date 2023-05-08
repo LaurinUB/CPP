75a772dd847717aa771e63b11ba139ef20ac9ee8
LaurinUB <Laurinub@yahoo.de>
2023-05-08 13:58:26 +0200

    completed ex00

---
+++ CPP01/ex00/Zombie.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 10:19:32 by luntiet-          #+#    #+#             */
+/*   Updated: 2023/05/08 13:36:29 by luntiet-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_H
+# define ZOMBIE_H
+
+#include <iostream>
+
+#define WHITE		"\033[0m"
+#define RED         "\033[31m"
+#define GREEN       "\033[32m"
+#define YELLOW      "\033[33m"
+#define PURPLE      "\033[35m"
+
+class	Zombie
+{
+	public :
+		
+		Zombie(void);
+		~Zombie(void);
+		Zombie(std::string name);
+
+		void	announce(void);
+
+	private :
+
+		std::string	name_;
+};
+
+void	randomChump(std::string name);
+Zombie	*newZombie(std::string name);
+
+#endif
