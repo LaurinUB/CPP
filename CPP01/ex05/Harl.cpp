f927446129c316ae53af2b054f0520b20f7cde71
LaurinUB <Laurinub@yahoo.de>
2023-05-13 14:09:24 +0200

    change printouts

--- CPP01/ex05/Harl.cpp
+++ CPP01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 08:43:05 by luntiet-          #+#    #+#             */
-/*   Updated: 2023/05/13 13:18:52 by luntiet-         ###   ########.fr       */
+/*   Updated: 2023/05/13 14:08:15 by luntiet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,26 +32,30 @@ Harl::~Harl(void)
 
 void	Harl::debug(void)
 {
-	std::cout << GREEN "debug:" << std::endl;
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" WHITE << std::endl;
+	std::cout << GREEN "[ DEBUG ]" << std::endl;
+	std::cout << "I love having extra bacon for my 8XL-double-cheese-triple-pickle-specialketchup burger." << std::endl;
+	std::cout << "I really do!" WHITE << std::endl << std::endl;
 }
 
 void	Harl::info(void)
 {
-	std::cout << GREEN "info:" << std::endl;
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" WHITE << std::endl;
+	std::cout << GREEN "[ INFO ]" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money." << std::endl;
+	std::cout << "You didn’t put enough bacon in my burger!" << std::endl; 
+	std::cout <<"If you did, I wouldn’t be asking for more!" WHITE << std::endl << std::endl;
 }
 
 void	Harl::warning(void)
 {
-	std::cout << GREEN "warning:" << std::endl;
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." WHITE << std::endl;
+	std::cout << GREEN "[ WARNING ]" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free." << std::endl;
+	std::cout << "I’ve been coming for years whereas you started working here since last month." WHITE << std::endl << std::endl;
 }
 
 void	Harl::error(void)
 {
-	std::cout << GREEN "error:" << std::endl;
-	std::cout << "This is unacceptable! I want to speak to the manager now." WHITE << std::endl;
+	std::cout << GREEN "[ ERROR ]" << std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now." WHITE << std::endl << std::endl;
 }
 
 void	Harl::complain(std::string level)
@@ -63,5 +67,5 @@ void	Harl::complain(std::string level)
 			return;
 		}
 
-	std::cout << RED "No level know as " << level << WHITE << std::endl;
+	std::cout << RED "This [ " << level << " ] is not a complain!" WHITE << std::endl;
 }
