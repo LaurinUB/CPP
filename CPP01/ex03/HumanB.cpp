8b90574bd15d506860ca9939944c98ccccbac618
LaurinUB <Laurinub@yahoo.de>
2023-05-13 14:29:02 +0200

    returning constant string intead of normal string

--- CPP01/ex03/HumanB.cpp
+++ CPP01/ex03/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 15:19:55 by luntiet-          #+#    #+#             */
-/*   Updated: 2023/05/10 18:39:23 by luntiet-         ###   ########.fr       */
+/*   Updated: 2023/05/13 14:28:07 by luntiet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,21 +27,6 @@ HumanB::HumanB(std::string name): name_(name), weapon_(NULL)
 	return;
 }
 
-std::string	HumanB::getName(void)
-{
-	return (this->name_);
-}
-
-Weapon	*HumanB::getWeapon(void)
-{
-	return (this->weapon_);
-}
-
-void	HumanB::setName(std::string name)
-{
-	this->name_ = name;
-}
-
 void	HumanB::setWeapon(Weapon &weapon)
 {
 	this->weapon_ =	&weapon;
@@ -50,10 +35,10 @@ void	HumanB::setWeapon(Weapon &weapon)
 void	HumanB::attack(void)
 {
 	if (!this->weapon_)
-		std::cout << this->getName() << " has no weapon equiped" << std::endl;
+		std::cout << this->name_ << " has no weapon equiped" << std::endl;
 	else
 	{
-		std::cout << this->getName() << " attacks with their ";
-		std::cout << this->getWeapon()->getType() << std::endl;
+		std::cout << this->name_ << " attacks with their ";
+		std::cout << this->weapon_->getType() << std::endl;
 	}
 }
--- CPP01/ex03/Weapon.cpp
+++ CPP01/ex03/Weapon.cpp
@@ -6,7 +6,7 @@
 /*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 15:28:01 by luntiet-          #+#    #+#             */
-/*   Updated: 2023/05/10 15:32:03 by luntiet-         ###   ########.fr       */
+/*   Updated: 2023/05/13 14:23:59 by luntiet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,12 @@ Weapon::Weapon(std::string type): type_(type)
 	return;
 }
 
-std::string	Weapon::getType(void)
+const std::string&	Weapon::getType(void)
 {
 	return (this->type_);
 }
 
-void	Weapon::setType(std::string	type)
+void	Weapon::setType(const std::string	type)
 {
 	this->type_ = type;
 }
