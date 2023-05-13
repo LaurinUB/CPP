8c3fd181578cb1ec548c00de36d8ef1e97a1b558
LaurinUB <Laurinub@yahoo.de>
2023-05-13 13:21:00 +0200

    ex05 finished

--- CPP01/ex05/Harl.cpp
+++ CPP01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 08:43:05 by luntiet-          #+#    #+#             */
-/*   Updated: 2023/05/12 11:06:05 by luntiet-         ###   ########.fr       */
+/*   Updated: 2023/05/13 13:18:52 by luntiet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,31 +18,50 @@ Harl::Harl(void)
 	this->functions_[1] = &Harl::info;
 	this->functions_[2] = &Harl::warning;
 	this->functions_[3] = &Harl::error;
+
+	this->levels_[0] = "debug";
+	this->levels_[1] = "info";
+	this->levels_[2] = "warning";
+	this->levels_[3] = "error";
 }
+
+Harl::~Harl(void)
+{
+
+}
+
 void	Harl::debug(void)
 {
-	std::cout << "DEBUG" << std::endl;
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+	std::cout << GREEN "debug:" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" WHITE << std::endl;
 }
 
 void	Harl::info(void)
 {
-	std::cout << "INFO" << std::endl;
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+	std::cout << GREEN "info:" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" WHITE << std::endl;
 }
 
 void	Harl::warning(void)
 {
-	std::cout << "WARNING" << std::endl;
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+	std::cout << GREEN "warning:" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." WHITE << std::endl;
 }
 
 void	Harl::error(void)
 {
-	std::cout << "ERROR" << std::endl;
-	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << GREEN "error:" << std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now." WHITE << std::endl;
 }
 
 void	Harl::complain(std::string level)
 {
+	for (int i = 0; i < 4; i++)
+		if (!level.compare(levels_[i]))
+		{
+			(this->*functions_[i])();
+			return;
+		}
+
+	std::cout << RED "No level know as " << level << WHITE << std::endl;
 }
--- CPP01/ex05/main.cpp
+++ CPP01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 08:38:16 by luntiet-          #+#    #+#             */
-/*   Updated: 2023/05/12 08:44:26 by luntiet-         ###   ########.fr       */
+/*   Updated: 2023/05/13 13:19:47 by luntiet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 
 int	main(void)
 {
+	Harl	harl;
 	
+	harl.complain("warning");
+	harl.complain("info");
+	harl.complain("debug");
+	harl.complain("error");
+	harl.complain("what");
 }
