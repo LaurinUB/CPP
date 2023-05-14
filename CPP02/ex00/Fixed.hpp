ee9c5e5479620f47fae95b98c61ece0f287741fb
LaurinUB <Laurinub@yahoo.de>
2023-05-14 20:11:09 +0200

    cononical setup

--- CPP02/ex00/Fixed.hpp
+++ CPP02/ex00/Fixed.hpp
@@ -6,12 +6,14 @@
 /*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 18:32:11 by luntiet-          #+#    #+#             */
-/*   Updated: 2023/05/13 18:38:26 by luntiet-         ###   ########.fr       */
+/*   Updated: 2023/05/14 20:09:00 by luntiet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_H
 # define FIXED_H
+
+#include <iostream>
 
 class Fixed
 {
@@ -19,12 +21,18 @@ class Fixed
 
 		Fixed(void);
 		~Fixed(void);
+		Fixed(Fixed const & fix);
+		Fixed & operator = (Fixed const & fix);
+
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
 
 	private:
 
 		int	fixed_point_number_;
 		static const int literal_ = 8;
-
 };
+
+std::ostream & operator << (std::ostream & o, Fixed const & f);
 
 #endif
