c71c24d41240a8d9144f392de588b3f4908b0022
LaurinUB <Laurinub@yahoo.de>
2023-05-14 20:11:09 +0200

    cononical setup

--- CPP02/ex00/Fixed.cpp
+++ CPP02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 18:37:19 by luntiet-          #+#    #+#             */
-/*   Updated: 2023/05/13 18:38:08 by luntiet-         ###   ########.fr       */
+/*   Updated: 2023/05/14 20:10:25 by luntiet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,31 @@
 Fixed::Fixed(void)
 {
 	fixed_point_number_ = 0;
+}
+
+Fixed::~Fixed(void)
+{
+	return;
+}
+
+Fixed::Fixed(Fixed const & fix)
+{
+	*this = fix;
+}
+
+Fixed & Fixed::operator = (Fixed const & fix)
+{
+	if (this != &fix)
+		this->fixed_point_number_ = fix.getRawBits();
+	return (*this);
+}
+
+int	Fixed::getRawBits(void) const
+{
+	return (this->fixed_point_number_);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	this->fixed_point_number_ = raw;
 }
