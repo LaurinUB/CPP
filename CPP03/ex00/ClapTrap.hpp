21d55337626276430ac44454f991926abb9203a4
LaurinUB <Laurinub@yahoo.de>
2023-05-17 21:21:11 +0200

    ex00 done

---
+++ CPP03/ex00/ClapTrap.hpp
@@ -0,0 +1,25 @@
+#ifndef CLAPTRAP_H
+#define CLAPTRAP_H
+ 
+#include <iostream>
+ 
+class ClapTrap {
+	public: 
+		ClapTrap(void); 
+		ClapTrap(const ClapTrap &rhs); 
+		ClapTrap(const std::string name);
+		~ClapTrap(void); 
+		ClapTrap& operator=(const ClapTrap &rhs); 
+
+		void		attack(const std::string& target);
+		void		takeDamage(unsigned int amount);
+		void		beRepaired(unsigned int amount);
+ 
+	private: 
+		std::string	name_;
+		int	hit_points_;
+		int	energy_points_;
+		int	attack_damage_;
+}; 
+ 
+#endif
