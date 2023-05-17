d49b123285bdccb0615610b094bb9bf5ada70047
LaurinUB <Laurinub@yahoo.de>
2023-05-17 22:44:09 +0200

    ex01 setup

---
+++ CPP03/ex00/ScavTrap.hpp
@@ -0,0 +1,19 @@
+#ifndef SCAVTRAP_H
+#define SCAVTRAP_H
+ 
+#include "ClapTrap.hpp"
+#include <iostream>
+ 
+class ScavTrap: public ClapTrap {
+	public: 
+		ScavTrap(void); 
+		ScavTrap(const ScavTrap &rhs); 
+		~ScavTrap(void); 
+		ScavTrap& operator=(const ScavTrap &rhs); 
+
+		void		attack(const std::string& target);
+		void		guardGate(void);
+ 
+}; 
+ 
+#endif
