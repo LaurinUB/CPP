c0fd3d28175865828f2af6944d827bafdddf9e70
LaurinUB <Laurinub@yahoo.de>
2023-05-18 12:15:38 +0200

    finished ex01

--- CPP03/ex01/ScavTrap.hpp
+++ CPP03/ex01/ScavTrap.hpp
@@ -1,6 +1,19 @@
-#pragma once
-
 #ifndef SCAVTRAP_H
 #define SCAVTRAP_H
+
+#include "ClapTrap.hpp"
+#include <iostream>
+
+class ScavTrap : public ClapTrap {
+  public:
+    ScavTrap(void);
+    ScavTrap(const std::string name);
+    ScavTrap(const ScavTrap &rhs);
+    ~ScavTrap(void);
+    ScavTrap& operator=(const ScavTrap &rhs);
+
+    void  attack(const std::string& target);
+    void  guardGate(void);
+};
 
 #endif
--- CPP03/ex01/main.cpp
+++ CPP03/ex01/main.cpp
@@ -1,10 +1,27 @@
-#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
 
-int	main(void) {
-	ClapTrap clap = ClapTrap("clap");
+int main(void) {
+  ClapTrap clap = ClapTrap("clap");
+  ScavTrap scav = ScavTrap("scav");
 
-	clap.attack("notClap");
-	clap.takeDamage(5);
-	clap.beRepaired(5);
-	return 0;
+  clap.attack("notClap");
+  clap.attack("notClap");
+  clap.attack("notClap");
+  clap.attack("notClap");
+  clap.takeDamage(5);
+  clap.beRepaired(5);
+  clap.attack("notClap");
+  clap.attack("notClap");
+  clap.attack("notClap");
+  clap.attack("notClap");
+  clap.attack("notClap");
+  clap.attack("notClap");
+  clap.beRepaired(5);
+
+  scav.attack("notScav");
+  scav.takeDamage(40);
+  scav.beRepaired(100);
+  scav.guardGate();
+
+  return 0;
 }
