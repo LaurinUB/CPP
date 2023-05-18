18b857a09cb4051755c75276e673b143583a515c
LaurinUB <Laurinub@yahoo.de>
2023-05-18 11:39:58 +0200

    style and 0 protection

--- CPP03/ex00/main.cpp
+++ CPP03/ex00/main.cpp
@@ -1,10 +1,10 @@
 #include "ClapTrap.hpp"
 
 int	main(void) {
-	ClapTrap clap = ClapTrap("clap");
+  ClapTrap clap = ClapTrap("clap");
 
-	clap.attack("notClap");
-	clap.takeDamage(5);
-	clap.beRepaired(5);
-	return 0;
+  clap.attack("notClap");
+  clap.takeDamage(5);
+  clap.beRepaired(5);
+  return 0;
 }
