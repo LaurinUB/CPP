a3e1295d49280197654652c4cbb25f172e608015
LaurinUB <Laurinub@yahoo.de>
2023-05-22 10:48:31 +0200

    ex00 done

---
+++ CPP04/ex00/Animal.cpp
@@ -0,0 +1,24 @@
+#include "Animal.hpp"
+
+Animal::Animal(void) : type_("default") {}
+
+Animal::Animal(std::string type) : type_(type) {}
+
+Animal::Animal(const Animal& rhs) {
+  *this = rhs;
+}
+
+Animal::~Animal() {}
+
+Animal&  Animal::operator=(const Animal& rhs) {
+  this->type_ = rhs.type_;
+  return *this;
+}
+
+std::string Animal::getType(void) const {
+  return this->type_;
+}
+
+void  Animal::makeSound(void) const {
+  std::cout << "not an animal" << std::endl << std::endl;
+}
---
+++ CPP04/ex00/Dog.cpp
@@ -0,0 +1,20 @@
+#include "Dog.hpp"
+
+Dog::Dog(void) {
+  this->type_ = "Dog";
+}
+
+Dog::Dog(const Dog& rhs) {
+  *this = rhs;
+}
+
+Dog::~Dog() {}
+
+Dog&  Dog::operator=(const Dog& rhs) {
+  this->type_ = rhs.type_;
+  return *this;
+}
+
+void  Dog::makeSound(void) const {
+  std::cout << "Wuff" << std::endl << std::endl;
+}
