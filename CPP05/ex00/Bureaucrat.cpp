0220fb78fff819de3567aff8912edbffcf14863f
LaurinUB <Laurinub@yahoo.de>
2023-05-26 13:30:33 +0200

    first compilable state

--- CPP05/ex00/Bureaucrat.cpp
+++ CPP05/ex00/Bureaucrat.cpp
@@ -6,15 +6,21 @@
 /*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 14:37:45 by luntiet-          #+#    #+#             */
-/*   Updated: 2023/05/25 15:12:13 by luntiet-         ###   ########.fr       */
+/*   Updated: 2023/05/26 13:00:36 by luntiet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat(void) : name_("default"), grade_(1) {
+Bureaucrat::Bureaucrat(void)
+    : name_("default"), grade_(1) {
   std::cout << "Bureaucrat default constructor called" << std::endl;
 }
+
+Bureaucrat::Bureaucrat(const std::string name, unsigned int grade)
+  : name_(name) {
+    
+  }
 
 Bureaucrat::Bureaucrat(const Bureaucrat& rhs) {
   *this = rhs;
@@ -35,4 +41,20 @@ std::ostream& operator<<(std::ostream& o, Bureaucrat b) {
   o << "Bureaucrat: " << b.getName() << " has the grade: "
     << b.getGrade() << std::endl;
   return o;
+}
+
+const std::string Bureaucrat::getName(void) const {
+  return this->name_;
+}
+
+unsigned int Bureaucrat::getGrade(void) const {
+  return this->grade_;
+}
+
+void  Bureaucrat::increment(void) {
+  this->grade_--;
+}
+
+void  Bureaucrat::decrement(void) {
+  this->grade_++;
 }
--- CPP05/ex00/Bureaucrat.hpp
+++ CPP05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 14:38:16 by luntiet-          #+#    #+#             */
-/*   Updated: 2023/05/25 15:12:55 by luntiet-         ###   ########.fr       */
+/*   Updated: 2023/05/26 13:00:07 by luntiet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,26 @@
 
 class Bureaucrat {
  public:
-  class GradeToHighException : public std::exception {};
-  class GradeTooLowException : public std::exception {};
+  class GradeToHighException : public std::exception {
+    virtual const char* what(void) const throw() {
+      return "the grade is to high!";
+    }
+  };
+  class GradeTooLowException : public std::exception {
+    virtual const char* what(void) const throw() {
+      return "the grade is to low!";
+    }
+  };
   Bureaucrat(void);
   Bureaucrat(const Bureaucrat &rhs);
   ~Bureaucrat(void);
+  Bureaucrat(const std::string name, unsigned int grade);
   Bureaucrat& operator=(const Bureaucrat &rhs);
 
   const std::string getName(void) const;
   unsigned int getGrade(void) const;
-  void  increment(Bureaucrat b);
-  void  decrement(Bureaucrat b);
+  void  increment(void);
+  void  decrement(void);
 
 
  private:
