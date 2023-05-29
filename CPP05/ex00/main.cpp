584e57f6a8cc5fb3f23087ee1cf8bae1c340bb5e
LaurinUB <Laurinub@yahoo.de>
2023-05-29 07:27:16 +0200

    better testing

--- CPP05/ex00/main.cpp
+++ CPP05/ex00/main.cpp
@@ -6,22 +6,29 @@
 /*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 14:37:55 by luntiet-          #+#    #+#             */
-/*   Updated: 2023/05/26 14:46:39 by luntiet-         ###   ########.fr       */
+/*   Updated: 2023/05/29 07:26:27 by luntiet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
 int main(void) {
+  std::cout << "Test1:" << std::endl;
   try {
     std::cout << "trying to initiate with Bureaucrat(\"Bob\", 10)"
       << std::endl;
     Bureaucrat bob = Bureaucrat("Bob", 10);
     std::cout << bob;
+    std::cout << "calling increment()" << std::endl;
+    bob.increment();
+    std::cout << bob;
+    std::cout << "calling decrement()" << std::endl;
+    bob.decrement();
+    std::cout << bob;
   } catch (std::exception& e) {
     std::cout << e.what() << std::endl;
   }
-  std::cout << std::endl;
+  std::cout << std::endl << "Test2:" << std::endl;
   try {
     std::cout << "trying to initiate with Bureaucrat(\"ToHigh\", 0)"
       << std::endl;
@@ -30,11 +37,33 @@ int main(void) {
   } catch (std::exception& e) {
     std::cout << e.what() << std::endl;
   }
-  std::cout << std::endl;
+  std::cout << std::endl << "Test3:" << std::endl;
+  try {
+    std::cout << "trying to initiate with Bureaucrat(\"Increment\", 1)"
+      << " and increment" << std::endl;
+    Bureaucrat toHigh = Bureaucrat("Increment", 1);
+    std::cout << toHigh;
+    std::cout << "calling increment()" << std::endl;
+    toHigh.increment();
+  } catch (std::exception& e) {
+    std::cout << e.what() << std::endl;
+  }
+  std::cout << std::endl << "Test4:" << std::endl;
+  try {
+    std::cout << "trying to initiate with Bureaucrat(\"Decrement\", 150)"
+      << " and decrement" << std::endl;
+    Bureaucrat toLow = Bureaucrat("Decrement", 150);
+    std::cout << toLow;
+    std::cout << "calling decrement()" << std::endl;
+    toLow.decrement();
+  } catch (std::exception& e) {
+    std::cout << e.what() << std::endl;
+  }
+  std::cout << std::endl << "Test5:" << std::endl;
   try {
     std::cout << "trying to initiate with Bureaucrat(\"toLow\", 200)"
       << std::endl;
-    Bureaucrat toLow = Bureaucrat("ToHigh", 200);
+    Bureaucrat toLow = Bureaucrat("ToLow", 200);
     std::cout << toLow << std::endl;
   } catch (std::exception& e) {
     std::cout << e.what() << std::endl;
