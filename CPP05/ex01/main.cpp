f58aee20b0c5ffbdcb8d1d24c00f90198a4c48f6
LaurinUB <Laurinub@yahoo.de>
2023-05-29 07:27:16 +0200

    better testing

--- CPP05/ex01/main.cpp
+++ CPP05/ex01/main.cpp
@@ -6,13 +6,14 @@
 /*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 14:37:55 by luntiet-          #+#    #+#             */
-/*   Updated: 2023/05/29 07:13:52 by luntiet-         ###   ########.fr       */
+/*   Updated: 2023/05/29 07:16:02 by luntiet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
 int main(void) {
+  std::cout << std::endl << "Test1:" << std::endl;
   try {
     std::cout << "trying to create Form with Form(\"test\", 200, 200)"
       << std::endl;
@@ -20,7 +21,7 @@ int main(void) {
   } catch (std::exception& e) {
     std::cout << e.what() << std::endl;
   }
-  std::cout << std::endl;
+  std::cout << std::endl << "Test2:" << std::endl;
   try {
     std::cout << "trying to create Form with Form(\"test\", 0, 0)"
       << std::endl;
@@ -28,15 +29,15 @@ int main(void) {
   } catch (std::exception& e) {
     std::cout << e.what() << std::endl;
   }
-  std::cout << std::endl;
+  std::cout << std::endl << "Test3:" << std::endl;
   try {
     std::cout << "trying to initiate with Bureaucrat(\"Bob\", 10)"
       << std::endl;
     Bureaucrat bob = Bureaucrat("Bob", 10);
-    std::cout << bob;
+    std::cout << bob << std::endl;
     std::cout << "creating Form with Form(\"test\", 20, 20)" << std::endl;
     Form form = Form("test", 20, 20);
-    std::cout << form;
+    std::cout << form << std::endl;
     std::cout << "signing the Form" << std::endl;
     form.beSigned(bob);
     std::cout << form;
