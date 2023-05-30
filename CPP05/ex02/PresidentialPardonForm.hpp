1aac3047262051c52bd73b498e447f681cf925af
LaurinUB <Laurinub@yahoo.de>
2023-05-30 18:42:32 +0200

    Presidential and Robotomy form and tests done

--- CPP05/ex02/PresidentialPardonForm.hpp
+++ CPP05/ex02/PresidentialPardonForm.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PresidentialPardonForm.hpp                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/30 14:00:03 by luntiet-          #+#    #+#             */
+/*   Updated: 2023/05/30 14:41:49 by luntiet-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PRESIDENTIALPARDONFORM_HPP_
 #define PRESIDENTIALPARDONFORM_HPP_
 
@@ -12,6 +24,8 @@ class PresidentialPardonForm : public Form {
   ~PresidentialPardonForm(void);
   PresidentialPardonForm& operator=(const PresidentialPardonForm &rhs);
   PresidentialPardonForm(const std::string name);
+
+  void  execute(const Bureaucrat& executer) const;
 };
 
 #endif  // PRESIDENTIALPARDONFORM_HPP_
--- CPP05/ex02/RobotomyRequestForm.hpp
+++ CPP05/ex02/RobotomyRequestForm.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RobotomyRequestForm.hpp                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/30 14:00:07 by luntiet-          #+#    #+#             */
+/*   Updated: 2023/05/30 14:41:39 by luntiet-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef ROBOTOMYREQUESTFORM_HPP_
 #define ROBOTOMYREQUESTFORM_HPP_
 
@@ -12,6 +24,8 @@ class RobotomyRequestForm : public Form {
   ~RobotomyRequestForm(void);
   RobotomyRequestForm& operator=(const RobotomyRequestForm &rhs);
   RobotomyRequestForm(const std::string name);
+
+  void  execute(const Bureaucrat& executer) const;
 };
 
 #endif  // ROBOTOMYREQUESTFORM_HPP_
