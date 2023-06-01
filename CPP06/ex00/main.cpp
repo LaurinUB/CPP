0f7df483b35e6b31913c8f5c79007bd589ee620d
LaurinUB <Laurinub@yahoo.de>
2023-06-01 22:57:12 +0200

    int working

--- CPP06/ex00/main.cpp
+++ CPP06/ex00/main.cpp
@@ -6,14 +6,18 @@
 /*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 09:34:44 by luntiet-          #+#    #+#             */
-/*   Updated: 2023/06/01 13:29:18 by luntiet-         ###   ########.fr       */
+/*   Updated: 2023/06/01 21:35:50 by luntiet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
 int main(int argc, char **argv) {
-  if (argc != 2) {
+  if (argc < 2) {
+    std::cout << "Please put in a Value." << std::endl;
+    return 1;
+  } else if (argc > 2) {
+    std::cout << "To many arguments!" << std::endl;
     return 1;
   }
   ScalarConverter::convert(argv[1]);
