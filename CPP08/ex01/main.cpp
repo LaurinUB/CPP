7eda20680f0982b3a15c2e436e313c97a9e89202
LaurinUB <Laurinub@yahoo.de>
2023-06-12 16:58:21 +0200

    save

--- CPP08/ex01/main.cpp
+++ CPP08/ex01/main.cpp
@@ -6,10 +6,16 @@
 /*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 19:00:32 by luntiet-          #+#    #+#             */
-/*   Updated: 2023/06/09 19:00:42 by luntiet-         ###   ########.fr       */
+/*   Updated: 2023/06/12 15:22:53 by luntiet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <vector>
+#include "Span.hpp"
+
 int main(void) {
+  Span test(12);
+  test.addNumber(3);
   return 0;
 }
