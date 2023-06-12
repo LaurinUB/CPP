5e360e877d3eb3aa20641c02ff576fe3a8d8c21d
LaurinUB <Laurinub@yahoo.de>
2023-06-12 16:58:21 +0200

    save

--- CPP08/ex01/Span.cpp
+++ CPP08/ex01/Span.cpp
@@ -6,13 +6,19 @@
 /*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 19:02:25 by luntiet-          #+#    #+#             */
-/*   Updated: 2023/06/09 19:06:55 by luntiet-         ###   ########.fr       */
+/*   Updated: 2023/06/12 16:56:21 by luntiet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 
-Span::Span() {}
+Span::Span() {
+  this->container_.reserve(0);
+}
+
+Span::Span(unsigned int N) {
+  this->container_.reserve(N);
+}
 
 Span::~Span() {}
 
@@ -25,4 +31,20 @@ Span& Span::operator=(const Span& obj) {
     *this = obj;
   }
   return *this;
+}
+
+void  Span::addNumber(int nbr) {
+  this->container_.push_back(nbr);
+}
+
+int Span::getNumber(unsigned int index) {
+  return this->container_[index];
+}
+
+int Span::shortestSpan() {
+  return 0;
+}
+
+int Span::longestSpan() {
+  return 0;
 }
--- CPP08/ex01/Span.hpp
+++ CPP08/ex01/Span.hpp
@@ -6,16 +6,19 @@
 /*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 19:02:27 by luntiet-          #+#    #+#             */
-/*   Updated: 2023/06/09 19:12:01 by luntiet-         ###   ########.fr       */
+/*   Updated: 2023/06/12 13:34:46 by luntiet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SPAN_HPP_
 #define SPAN_HPP_
 
+#include <vector>
+
 class Span {
  public:
   Span();
+  Span(unsigned int N);
   ~Span();
   Span(const Span& obj);
   Span& operator=(const Span& obj);
@@ -24,7 +27,10 @@ class Span {
   int shortestSpan();
   int longestSpan();
 
+  int getNumber(unsigned int index);
+
  private:
+  std::vector<int> container_;
 };
 
 #endif  // SPAN_HPP_
