2c1a0fb7034bfec0c3fd4957a5efd2832e2d74d5
LaurinUB <Laurinub@yahoo.de>
2023-06-13 11:22:36 +0200

    finished ex01

--- CPP08/ex01/main.cpp
+++ CPP08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 19:00:32 by luntiet-          #+#    #+#             */
-/*   Updated: 2023/06/12 15:22:53 by luntiet-         ###   ########.fr       */
+/*   Updated: 2023/06/13 11:21:48 by luntiet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,38 @@
 #include "Span.hpp"
 
 int main(void) {
-  Span test(12);
-  test.addNumber(3);
+  std::cout << "Test from subject" << std::endl;
+  Span sp = Span(5);
+
+  sp.addNumber(6);
+  sp.addNumber(3);
+  sp.addNumber(17);
+  sp.addNumber(9);
+  sp.addNumber(11);
+  std::cout << sp.shortestSpan() << std::endl;
+  std::cout << sp.longestSpan() << std::endl;
+
+  std::cout << std::endl << "own tests" << std::endl;
+  Span test(3);
+  std::cout << "try shortestSpan call without elements:" << std::endl;
+  try {
+    std::cout << test.shortestSpan() << std::endl;
+  } catch (std::exception &e) {
+    std::cerr << e.what() << std::endl;
+  }
+  test.addNumber(2);
+  std::cout << "try shortestSpan call with one element:" << std::endl;
+  try {
+    std::cout << test.shortestSpan() << std::endl;
+  } catch (std::exception &e) {
+    std::cerr << e.what() << std::endl;
+  }
+  test.addNumber(1);
+  std::cout << "try shortestSpan call with two element:" << std::endl;
+  try {
+    std::cout << test.shortestSpan() << std::endl;
+  } catch (std::exception &e) {
+    std::cerr << e.what() << std::endl;
+  }
   return 0;
 }
