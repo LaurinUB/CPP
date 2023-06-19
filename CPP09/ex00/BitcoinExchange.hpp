3a6a4d7cd20540159226fdc32217432ce1416fcd
LaurinUB <Laurinub@yahoo.de>
2023-06-19 16:15:32 +0200

    finished ex00

--- CPP09/ex00/BitcoinExchange.hpp
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -6,12 +6,14 @@
 /*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 17:43:35 by luntiet-          #+#    #+#             */
-/*   Updated: 2023/06/19 10:54:19 by luntiet-         ###   ########.fr       */
+/*   Updated: 2023/06/19 16:13:33 by luntiet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BITCOINEXCHANGE_HPP_
 #define BITCOINEXCHANGE_HPP_
+
+#define DATA "data.csv"
 
 #include <string>
 #include <iostream>
@@ -27,6 +29,12 @@ class BitcoinExchange {
   static void exchange(std::string infile);
 
  private:
+  static void parse_infile(std::string infile, std::map<std::string, float> data);
+  static float  find_value(std::string tmpDate, float tmpVal, std::map<std::string, float> data);
+  static float  to_float(std::string str);
+  static std::map<std::string, float> parse_database();
+  static bool validate_date(std::string date);
+  static float validate_val(std::string val);
   BitcoinExchange();
   ~BitcoinExchange();
   BitcoinExchange(const BitcoinExchange& obj);
