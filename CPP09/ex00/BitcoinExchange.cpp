1bc8276f63ff3c10f3cc4810ce24d9c5a2f85de8
LaurinUB <Laurinub@yahoo.de>
2023-06-19 16:15:32 +0200

    finished ex00

--- CPP09/ex00/BitcoinExchange.cpp
+++ CPP09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 18:20:31 by luntiet-          #+#    #+#             */
-/*   Updated: 2023/06/19 11:14:37 by luntiet-         ###   ########.fr       */
+/*   Updated: 2023/06/19 16:12:47 by luntiet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& obj) {
   return *this;
 }
 
-bool  validate_date(std::string date) {
+bool  BitcoinExchange::validate_date(std::string date) {
   std::tm tm = {};
 
   if (!strptime(date.c_str(), "%Y-%m-%d", &tm)) {
@@ -36,30 +36,35 @@ bool  validate_date(std::string date) {
   return true;
 }
 
-bool validate_val(std::string val) {
+float BitcoinExchange::validate_val(std::string val) {
   std::stringstream ss(val);
   float f;
 
   ss >> f;
   if (!ss.eof()) {
     std::cout << "Error: not a Float value." << std::endl;
-    return false;
+    return -1.0;
   } else if (f < 0.0) {
     std::cout << "Error: not a positive number." << std::endl;
-    return false;
+    return -1.0;
   } else if (f > 1000.0) {
     std::cout << "Error: too large a number." << std::endl;
-    return false;
+    return -1.0;
   }
-  return true;
+  return f;
 }
 
-void  parse_infile(std::string infile) {
+float BitcoinExchange::find_value(std::string tmpDate, float tmpVal, std::map<std::string, float> data) {
+  std::map<std::string, float>::iterator it;
+  it = data.lower_bound(tmpDate);
+  return (it->second * tmpVal);
+}
+
+void  BitcoinExchange::parse_infile(std::string infile, std::map<std::string, float> data) {
   std::ifstream in;
   std::string tmp;
   std::string tmpDate;
   std::string tmpVal;
-  std::map<std::string, float> output;
   float f;
 
   in.open(infile.c_str());
@@ -80,17 +85,45 @@ void  parse_infile(std::string infile) {
       continue;
     }
     in >> tmpVal;
-    if (validate_val(tmpVal)) {
-      std::stringstream ss(tmpVal);
-      ss >> f;
-      output.insert(make_pair(tmpDate, f));
-      std::cout << tmpDate << " => ";
-      std::cout << output.at(tmpDate) << std::endl;
+    f = validate_val(tmpVal);
+    if (f >= 0.0 && f <= 1000.0) {
+      std::cout << tmpDate << " => " <<  tmpVal << " = "
+        << find_value(tmpDate, f, data) << std::endl;
     }
     in >> tmpDate;
   }
 }
 
+float BitcoinExchange::to_float(std::string str) {
+  std::istringstream iss(str);
+  float f;
+
+  iss >> f;
+  return f;
+}
+
+std::map<std::string, float> BitcoinExchange::parse_database() {
+  std::ifstream data;
+  std::map<std::string, float> res;
+  std::string tmp;
+  std::string date;
+
+  data.open(DATA);
+  if (!data.is_open()) {
+    std::cout << "Error: could not find database." << std::endl;
+  }
+  data >> tmp;
+  while (!data.eof()) {
+    data >> tmp;
+    date = tmp.substr(0, tmp.find(','));
+    tmp = tmp.substr(tmp.find(',') + 1, tmp.size());
+    res.insert(make_pair(date, to_float(tmp)));
+  }
+  return res;
+}
+
 void  BitcoinExchange::exchange(std::string infile) {
-  parse_infile(infile);
+  std::map<std::string, float> data;
+  data = parse_database();
+  parse_infile(infile, data);
 }
--- CPP09/ex00/main.cpp
+++ CPP09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 17:16:17 by luntiet-          #+#    #+#             */
-/*   Updated: 2023/06/16 14:54:05 by luntiet-         ###   ########.fr       */
+/*   Updated: 2023/06/19 16:14:38 by luntiet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
-    std::cout << "please input like: [ ./btc input.txt ]" << std::endl;
+    std::cout << "Error: please input like: [ ./btc inputfile ]" << std::endl;
     return 1;
   }
   BitcoinExchange::exchange(argv[1]);
