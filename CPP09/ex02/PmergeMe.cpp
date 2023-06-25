98748183203626105fc5645647b593788e2444ec
LaurinUB <Laurinub@yahoo.de>
2023-06-25 20:08:48 +0200

    new pairs sort working for vec but not for deq

--- CPP09/ex02/PmergeMe.cpp
+++ CPP09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 12:49:05 by luntiet-          #+#    #+#             */
-/*   Updated: 2023/06/25 15:47:51 by luntiet-         ###   ########.fr       */
+/*   Updated: 2023/06/25 20:05:25 by luntiet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,8 +75,62 @@ std::vector<int>  PmergeMe::jacobsthalNbr(int argc) {
   return res;
 }
 
+std::vector< std::pair<int, int> > split_pairs_vec(const std::vector<int>& vec) {
+  std::vector< std::pair<int, int> > res;
+
+  if (vec.size() == 2) {
+    res.push_back(std::make_pair(vec[0], vec[1]));
+    return res;
+  }
+  for (size_t i = 0; i < vec.size(); ++i) {
+    if (i + 1 < vec.size()) {
+      if (vec[i] > vec[i + 1]) {
+        res.push_back(std::make_pair(vec[i], vec[i + 1]));
+        i++;
+      } else {
+        res.push_back(std::make_pair(vec[i + 1], vec[i]));
+        i++;
+      }
+    } else {
+      std::cout << "Error: Failed to split pairs." << std::endl;
+      exit(EXIT_FAILURE);
+    }
+  }
+  return res;
+}
+
+std::vector<int>  insert_sort_vec(std::vector< std::pair<int, int> > pairs, int unp) {
+  std::vector<int>  vec;
+  std::vector< std::pair<int, int> >::iterator it = pairs.begin();
+  std::vector<int> nbr = PmergeMe::jacobsthalNbr(pairs.size());
+
+  if (pairs.size() == 1) {
+    return sort_one(pairs, vec, unp);
+  }
+  vec.push_back(it->second);
+  for (size_t i = 0; i < pairs.size(); ++i) {
+    vec.push_back(it[i].first);
+  }
+  pairs.erase(it);
+  it = pairs.begin() + nbr[0];
+  for (size_t i = 0; i < nbr.size(); ++i) {
+    int t = binary_search(it->second, vec);
+    if (t == 0 && it->second > vec[t]) {
+      std::cout << "Error: could not find spot to insert." << std::endl;
+      exit(EXIT_FAILURE);
+    } else {
+      vec.insert(vec.begin() + t, it->second);
+    }
+    it = pairs.begin() + nbr[i];
+  }
+  if (unp != -1) {
+    vec.insert(vec.begin() + binary_search(unp, vec), unp);
+  }
+  return vec;
+}
+
 double  PmergeMe::sortVec(std::vector<int>& vec) {
-  std::vector<int> tmp;
+  int unpaired = -1;
   clock_t t;
 
   t = std::clock();
@@ -90,16 +144,44 @@ double  PmergeMe::sortVec(std::vector<int>& vec) {
     t = std::clock() - t;
     return (static_cast<double>(t) / CLOCKS_PER_SEC);
   }
-  split_pairs(tmp, vec);
-  sort_pairs(tmp, vec);
-  std::vector<int> nbr = jacobsthalNbr(tmp.size());
-  insert_sort(tmp, vec, nbr);
+  if (vec.size() % 2 != 0) {
+    unpaired = vec.back();
+    vec.pop_back();
+  }
+  std::vector< std::pair<int, int> > pairs = split_pairs_vec(vec);
+  pairs = sort_pairs(pairs);
+  // vec = insert_sort_vec(pairs, unpaired);
+  vec = insert_sort(pairs, vec, unpaired);
   t = std::clock() - t;
   return (static_cast<double>(t) / CLOCKS_PER_SEC);
 }
 
+std::deque< std::pair<int, int> > split_pairs_deq(const std::deque<int>& deq) {
+  std::deque< std::pair<int, int> > res;
+
+  if (deq.size() == 2) {
+    res.push_back(std::make_pair(deq[0], deq[1]));
+    return res;
+  }
+  for (size_t i = 0; i < deq.size(); ++i) {
+    if (i + 1 < deq.size()) {
+      if (deq[i] > deq[i + 1]) {
+        res.push_back(std::make_pair(deq[i], deq[i + 1]));
+        i++;
+      } else {
+        res.push_back(std::make_pair(deq[i + 1], deq[i]));
+        i++;
+      }
+    } else {
+      std::cout << "Error: Failed to split pairs." << std::endl;
+      exit(EXIT_FAILURE);
+    }
+  }
+  return res;
+}
+
 double  PmergeMe::sortDeq(std::deque<int>& deq) {
-  std::deque<int> tmp;
+  int unpaired = -1;
   clock_t t;
 
   t = std::clock();
@@ -113,10 +195,21 @@ double  PmergeMe::sortDeq(std::deque<int>& deq) {
     t = std::clock() - t;
     return (static_cast<double>(t) / CLOCKS_PER_SEC);
   }
-  split_pairs(tmp, deq);
-  sort_pairs(tmp, deq);
-  std::vector<int> nbr = jacobsthalNbr(tmp.size());
-  insert_sort(tmp, deq, nbr);
+  if (deq.size() % 2 != 0) {
+    unpaired = deq.back();
+    deq.pop_back();
+  }
+  std::deque< std::pair<int, int> > pairs = split_pairs_deq(deq);
+  for (size_t i = 0; i < pairs.size(); ++i) {
+    std::cout << "info: ";
+    std::cout << pairs[i].first << ", " << pairs[i].second<< std::endl;
+  }
+  pairs = sort_pairs(pairs);
+  for (size_t i = 0; i < pairs.size(); ++i) {
+    std::cout << "info 2 :";
+    std::cout << pairs[i].first << ", " << pairs[i].second<< std::endl;
+  }
+  insert_sort(pairs, deq, unpaired);
   t = std::clock() - t;
   return (static_cast<double>(t) / CLOCKS_PER_SEC);
 }
--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 12:49:43 by luntiet-          #+#    #+#             */
-/*   Updated: 2023/06/25 14:35:44 by luntiet-         ###   ########.fr       */
+/*   Updated: 2023/06/25 19:56:43 by luntiet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,20 @@
 #include <algorithm>
 #include <ctime>
 #include <utility>
+
+class PmergeMe {
+ public:
+  static double sortVec(std::vector<int>& vec);
+  static double sortDeq(std::deque<int>& deq);
+  static int validate_number(std::string val);
+  static std::vector<int> jacobsthalNbr(int argc);
+
+ private:
+  PmergeMe();
+  ~PmergeMe();
+  PmergeMe(const PmergeMe& obj);
+  PmergeMe& operator=(const PmergeMe& obj);
+};
 
 template<typename T>
 bool  is_sorted(const T& container) {
@@ -62,16 +76,20 @@ int binary_search(int nbr, const T& target) {
     m = (l + r) / 2;
     if (target[m] < nbr) {
       l = m + 1;
+      std::cout << "compare" << std::endl;
     } else if (target[m] > nbr) {
       r = m - 1;
+      std::cout << "compare" << std::endl;
     } else {
       return m;
     }
   }
-  if (target[m] >= nbr)
+  if (target[m] >= nbr) {
+    std::cout << "compare" << std::endl;
     return m;
-  else
+  } else {
     return m + 1;
+  }
 }
 
 template<typename T>
@@ -85,25 +103,6 @@ void  insert_at(typename T::iterator it, T& con) {
   }
 }
 
-template<typename T, typename S>
-void  insert_sort(T& tmp, T& con, const S& nbr) {
-  if (tmp.size() != con.size()) {
-    typename T::iterator last = tmp.begin() + tmp.size() - 2;
-    con.push_back(*last);
-    tmp.erase(last);
-  } else {
-    typename T::iterator last = tmp.begin() + tmp.size() - 1;
-    con.push_back(*last);
-    tmp.erase(last);
-  }
-  typename T::iterator it = tmp.begin() + nbr[0];
-  for (size_t i = 0; i < tmp.size(); ++i) {
-    insert_at(it, con);
-    it = tmp.begin() + nbr[i];
-  }
-}
-
-
 template<typename T>
 void  split_pairs(T& tmp, T& con) {
   typename T::iterator it1 = con.begin();
@@ -113,9 +112,11 @@ void  split_pairs(T& tmp, T& con) {
     if (*it2 <= *it1) {
       tmp.push_back(*it1);
       con.erase(it1);
+      std::cout << "compare" << std::endl;
     } else {
       tmp.push_back(*it2);
       con.erase(it2);
+      std::cout << "compare" << std::endl;
     }
     it2 = con.begin() + tmp.size() + 1;
     it1 = con.begin() + tmp.size();
@@ -127,42 +128,104 @@ void  split_pairs(T& tmp, T& con) {
 }
 
 template<typename T>
-void  sort_pairs(T& tmp, T& con) {
-  typename T::iterator first = con.begin();
-  typename T::iterator last = con.end();
-  typename T::iterator cr = tmp.begin() + 1;
-  typename T::iterator prv = tmp.begin();
+T merge(const T& left, const T& right) {
+  T res;
+  size_t  indexL = 0;
+  size_t  indexR = 0;
 
-  for (typename T::iterator sorted = first; first != last; last = sorted) {
-    sorted = first;
-    cr = tmp.begin() + 1;
-    prv = tmp.begin();
-    for (typename T::iterator cur = first, prev = first; ++cur != last; ++prev) {
-      if (*cur <= *prev) {
-        std::iter_swap(cur, prev);
-        if (cr != tmp.end()) {
-          std::iter_swap(cr, prv);
-        }
-        sorted = cur;
-      }
-      cr++;
-      prv++;
+  while (indexL < left.size() && indexR < right.size()) {
+    std::cout << "compare" << std::endl;
+    if (left[indexL].first < right[indexR].first) {
+      res.push_back(left[indexL]);
+      indexL++;
+    } else {
+      res.push_back(right[indexR]);
+      indexR++;
     }
   }
+  while (indexL < left.size()) {
+      res.push_back(left[indexL]);
+      indexL++;
+  }
+  while (indexR < right.size()) {
+      res.push_back(right[indexR]);
+      indexR++;
+  }
+  return res;
 }
 
-class PmergeMe {
- public:
-  static double sortVec(std::vector<int>& vec);
-  static double sortDeq(std::deque<int>& deq);
-  static int validate_number(std::string val);
-  static std::vector<int> jacobsthalNbr(int argc);
+template<typename P, typename T>
+T sort_one(const P& pairs, T con, int unp) {
+  if (pairs[0].first > pairs[0].second) {
+    con.push_back(pairs[0].second);
+    con.push_back(pairs[0].first);
+  } else {
+    con.push_back(pairs[0].first);
+    con.push_back(pairs[0].second);
+  }
+  if (unp != -1) {
+    con.insert(con.begin() + binary_search(unp, con), unp);
+  }
+  return con;
+}
 
- private:
-  PmergeMe();
-  ~PmergeMe();
-  PmergeMe(const PmergeMe& obj);
-  PmergeMe& operator=(const PmergeMe& obj);
-};
+template<typename P, typename T>
+T insert_sort(P pairs, T con, int unp) {
+  con.clear();
+  typename P::iterator it = pairs.begin();
+  std::vector<int> nbr = PmergeMe::jacobsthalNbr(pairs.size());
+
+  if (pairs.size() == 1) {
+    return sort_one(pairs, con, unp);
+  }
+  con.push_back(it->second);
+  for (size_t i = 0; i < pairs.size(); ++i) {
+    con.push_back(it[i].first);
+  }
+  pairs.erase(it);
+  it = pairs.begin() + nbr[0];
+  for (size_t i = 0; i < nbr.size(); ++i) {
+    int t = binary_search(it->second, con);
+    if (t == 0 && it->second > con[t]) {
+      std::cout << "Error: could not find spot to insert." << std::endl;
+      exit(EXIT_FAILURE);
+    } else {
+      con.insert(con.begin() + t, it->second);
+    }
+    it = pairs.begin() + nbr[i];
+  }
+  if (unp != -1) {
+    con.insert(con.begin() + binary_search(unp, con), unp);
+  }
+  return con;
+}
+
+template<typename T>
+T sort_pairs(T con) {
+  if (con.size() == 1) {
+    return con;
+  } else if (con.size() == 2) {
+    std::cout << "compare" << std::endl;
+    if (con[0].first > con[1].first) {
+      iter_swap(con.begin(), con.begin() + 1);
+    }
+    return con;
+  }
+
+  size_t mid = con.size() / 2;
+  T left;
+  T right;
+
+  for (size_t i = 0; i < mid; ++i) {
+    left.push_back(con[i]);
+  }
+  for (size_t i = mid; i < con.size(); ++i) {
+    right.push_back(con[i]);
+  }
+  T sortedRight = sort_pairs(right);
+  T sortedLeft = sort_pairs(left);
+
+  return merge(sortedRight, sortedLeft);
+}
 
 #endif  // PMERGEME_HPP_
